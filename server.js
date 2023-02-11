b0a3e1ed2d5fcb07890372ff60f7ade1276c90c9
Joowon <201611804@kyonggi.ac.kr>
2023-02-11 23:55:36 +0900

    fix server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = "127.0.0.1";
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
@@ -35,6 +35,6 @@ app.prepare().then(() => {
     }
   }).listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+    console.log(`> Ready on https://${hostname}:${port}`);
   });
 });
