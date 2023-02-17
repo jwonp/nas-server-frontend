8ffe40bef24523d4cd6a5ca60195ed45b7488e18
Joowon <201611804@kyonggi.ac.kr>
2023-02-18 08:54:01 +0900

    update nextjs to https

--- next.config.js
+++ next.config.js
@@ -15,12 +15,12 @@ const nextConfig = {
       },
       {
         source: "/login/authorize",
-        destination: `http://api.ikiningyou.com/users/o/authorize/?response_type=code&code_challenge=${process.env.NEXT_PUBLIC_CODE_CHALLENGE}&code_challenge_method=S256&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=http://www.ikiningyou.com/`,
+        destination: `https://api.ikiningyou.com/users/o/authorize/?response_type=code&code_challenge=${process.env.NEXT_PUBLIC_CODE_CHALLENGE}&code_challenge_method=S256&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=http://www.ikiningyou.com/`,
       },
       {
         source: "/login/authorize",
 
-        destination: `http://api.ikiningyou.com/users/o/authorize/?response_type=code&code_challenge=${process.env.NEXT_PUBLIC_CODE_CHALLENGE}&code_challenge_method=S256&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=http://www.ikiningyou.com/`,
+        destination: `https://api.ikiningyou.com/users/o/authorize/?response_type=code&code_challenge=${process.env.NEXT_PUBLIC_CODE_CHALLENGE}&code_challenge_method=S256&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=http://www.ikiningyou.com/`,
       },
       {
         source: "/kanata",
@@ -33,76 +33,77 @@ const nextConfig = {
       //APIs
       {
         source: "/test/authorizationview",
-        destination: "http://api.ikiningyou.com/authorizationview/",
+        destination: "https://api.ikiningyou.com/authorizationview/",
       },
       {
         source: "/test/boardlist",
-        destination: "http://api.ikiningyou.com/boardlist/",
+        destination: "https://api.ikiningyou.com/boardlist/",
       },
       {
         source: "/test/getStorageSize",
-        destination: "http://api.ikiningyou.com/users/getstoragesize/",
+        destination: "https://api.ikiningyou.com/users/getstoragesize/",
       },
       {
         source: "/test/getfolders",
-        destination: "http://api.ikiningyou.com/getfolders/",
+        destination: "https://api.ikiningyou.com/getfolders/",
       },
       {
         source: "/test/uploadfiles",
-        destination: "http://api.ikiningyou.com/users/uploadfiles/",
+        destination: "https://api.ikiningyou.com/users/uploadfiles/",
       },
       {
         source: "/test/downloadfiles",
-        destination: "http://api.ikiningyou.com/users/downloadfiles/",
+        destination: "https://api.ikiningyou.com/users/downloadfiles/",
       },
       {
         source: "/test/deletefiles",
-        destination: "http://api.ikiningyou.com/users/deletefiles/",
+        destination: "https://api.ikiningyou.com/users/deletefiles/",
       },
       {
         source: "/test/register",
-        destination: "http://api.ikiningyou.com/register/",
+        destination: "https://api.ikiningyou.com/register/",
       },
       {
         source: "/test/login",
-        destination: "http://api.ikiningyou.com/login/",
+        destination: "https://api.ikiningyou.com/login/",
       },
       {
         source: "/test/submitlogin",
-        destination: "http://api.ikiningyou.com/submitlogin/",
+        destination: "https://api.ikiningyou.com/submitlogin/",
       },
       {
         source: "/test/getuser",
-        destination: "http://api.ikiningyou.com/getuser/",
+        destination: "https://api.ikiningyou.com/getuser/",
       },
       {
         source: "/test/auth",
-        destination: "http://api.ikiningyou.com/users/o/authorize/",
+        destination: "https://api.ikiningyou.com/users/o/authorize/",
       },
       {
         source: "/test/token",
-        destination: "http://api.ikiningyou.com/users/o/token/",
+        destination: "https://api.ikiningyou.com/users/o/token/",
       },
       {
         source: "/test/logout",
-        destination: "http://api.ikiningyou.com/logout/",
+        destination: "https://api.ikiningyou.com/logout/",
       },
 
       {
         source: "/test/refresh",
-        destination: "http://api.ikiningyou.com/refreshtoken/",
+        destination: "https://api.ikiningyou.com/refreshtoken/",
       },
       {
         source: "/test/validtoken",
-        destination: "http://api.ikiningyou.com/users/validtoken/",
+        destination: "https://api.ikiningyou.com/users/validtoken/",
       },
       {
         source: "/test/getfilelistbypath/:path*",
-        destination: "http://api.ikiningyou.com/users/getfilelistbypath/:path*",
+        destination:
+          "https://api.ikiningyou.com/users/getfilelistbypath/:path*",
       },
       {
         source: "/test/addFolder/:path*",
-        destination: "http://api.ikiningyou.com/users/addfolder/",
+        destination: "https://api.ikiningyou.com/users/addfolder/",
       },
     ];
   },
