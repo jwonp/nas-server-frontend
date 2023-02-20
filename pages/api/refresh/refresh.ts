a83d61cf75ca182f25a401f5db19cf389754df44
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 22:49:38 +0900

    test

--- pages/api/refresh/refresh.ts
+++ pages/api/refresh/refresh.ts
@@ -7,19 +7,19 @@ export default async function refreshTokenHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // await axios
-  //   .post(
-  //     "https://api.ikiningyou.com/users/o/refresh/",
-  //     { data: "data" },
-  //     {
-  //       headers: {
-  //         'set-cookie': `refresh=${req.cookies.refresh}`,
-  //         withCredentials: true,
-  //       },
-  //     }
-  //   )
-  //   .then((response) => {
-  //     res.status(200).end(response.data);
-  //   });
-  res.status(200).end(req.cookies.refresh);
+  await axios
+    .post(
+      "https://api.ikiningyou.com/users/o/refresh/",
+      { data: "data" },
+      {
+        headers: {
+          Cookie: `refresh=${req.cookies.refresh}`,
+          withCredentials: true,
+        },
+      }
+    )
+    .then((response) => {
+      res.status(200).end(response.data);
+    });
+  res.status(200).end();
 }
