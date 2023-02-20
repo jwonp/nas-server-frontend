49ceba746935f3c408362cffc3c86d3820ff9427
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 12:47:52 +0900

    test logout

--- pages/api/revoke/[token].ts
+++ pages/api/revoke/[token].ts
@@ -7,23 +7,21 @@ export default function revokeTokenHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    if (!Object.hasOwn(req.body, "token")) res.status(403);
-    if (req.body.token === "") res.status(400);
+  if (!Object.hasOwn(req.body, "token")) res.status(403);
+  if (req.body.token === "") res.status(400);
 
-    revokeAccessToken(req.body.token, () => {});
-    // fetch("https://api.ikiningyou.com/users/o/revoke-token/", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/x-www-form-urlencoded",
-    //     "Cache-Control": "no-cache",
-    //   },
-    //   body: qs.stringify({
-    //     token: req.body.token,
-    //     client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
-    //   }),
-    // });
+  revokeAccessToken(req.body.token, () => {});
+  // fetch("https://api.ikiningyou.com/users/o/revoke-token/", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //     "Cache-Control": "no-cache",
+  //   },
+  //   body: qs.stringify({
+  //     token: req.body.token,
+  //     client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+  //   }),
+  // });
 
-    res.status(200);
-  }
+  res.status(200);
 }
