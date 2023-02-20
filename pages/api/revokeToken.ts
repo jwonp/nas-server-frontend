cf4be20cbffb7ffdad5613c09ed00e4a73605be8
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 12:30:52 +0900

    test logout

--- pages/api/revokeToken.ts
+++ pages/api/revokeToken.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
+import qs from "qs";
 import { revokeAccessToken } from "../../components/tools/requests";
 
 export default function revokeTokenHandler(
@@ -8,8 +9,20 @@ export default function revokeTokenHandler(
 ) {
   if (req.method === "POST") {
     if (!Object.hasOwn(req.body, "token")) res.status(403);
-    revokeAccessToken(req.body.token, (response) => {
-      res.status(200);
+    if (req.body.token === "") res.status(400);
+
+    fetch("/api/revokeToken", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Cache-Control": "no-cache",
+      },
+      body: qs.stringify({
+        token: req.body.token,
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+      }),
     });
+
+    res.status(200);
   }
 }
