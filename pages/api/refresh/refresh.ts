febc6713ef9f42af9ec6b680c0237ddce96a7a47
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 21:41:44 +0900

    test

--- pages/api/refresh/refresh.ts
+++ pages/api/refresh/refresh.ts
@@ -6,13 +6,6 @@ export default function refreshTokenHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  RefreshExpiredToken().then((res) => {
-    const { access_token, refresh_token } = res.data;
-    // res.setHeader(
-    //   "set-cookie",
-    //   `refresh=${refresh_token}; path=/; samesite=lax; httponly;`
-    // );
-    // res.status(200).json({ access_token: access_token });
-    res.status(200).end("refresh the token");
-  });
+  RefreshExpiredToken();
+  res.status(200).end("refresh the token");
 }
