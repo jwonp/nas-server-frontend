00605168d829512cd88fbb029f1e27b6a66f6760
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 12:46:37 +0900

    test logout

--- pages/api/revoke/[token].ts
+++ pages/api/revoke/[token].ts
@@ -11,18 +11,18 @@ export default function revokeTokenHandler(
     if (!Object.hasOwn(req.body, "token")) res.status(403);
     if (req.body.token === "") res.status(400);
 
-    // await revokeAccessToken(req.body.token,()=>)
-    fetch("https://api.ikiningyou.com/users/o/revoke-token/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cache-Control": "no-cache",
-      },
-      body: qs.stringify({
-        token: req.body.token,
-        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
-      }),
-    });
+    revokeAccessToken(req.body.token, () => {});
+    // fetch("https://api.ikiningyou.com/users/o/revoke-token/", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/x-www-form-urlencoded",
+    //     "Cache-Control": "no-cache",
+    //   },
+    //   body: qs.stringify({
+    //     token: req.body.token,
+    //     client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    //   }),
+    // });
 
     res.status(200);
   }
