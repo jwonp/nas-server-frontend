d08a0333082ba06ccf95da84d9b9394a65202268
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 12:45:50 +0900

    test logout

--- pages/api/revoke/[token].ts
+++ pages/api/revoke/[token].ts
@@ -11,7 +11,8 @@ export default function revokeTokenHandler(
     if (!Object.hasOwn(req.body, "token")) res.status(403);
     if (req.body.token === "") res.status(400);
 
-    fetch("/api/revokeToken", {
+    // await revokeAccessToken(req.body.token,()=>)
+    fetch("https://api.ikiningyou.com/users/o/revoke-token/", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
