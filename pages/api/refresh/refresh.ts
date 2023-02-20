af300ab8f8cdfb4f15f4a1c0f0353649156210f6
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 22:56:49 +0900

    test

--- pages/api/refresh/refresh.ts
+++ pages/api/refresh/refresh.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { resolve4 } from "dns/promises";
 import type { NextApiRequest, NextApiResponse } from "next";
+import qs from "qs";
 import { RefreshExpiredToken } from "../../../components/tools/httpClient";
 
 export default async function refreshTokenHandler(
@@ -10,12 +11,12 @@ export default async function refreshTokenHandler(
   await axios
     .post(
       "https://api.ikiningyou.com/users/o/refresh/",
-      {
+      qs.stringify({
         "grant_type": "refresh_token",
         "refresh_token": req.cookies.refresh,
         "client_id": process.env.NEXT_PUBLIC_CLIENT_ID,
         "client_secret": process.env.NEXT_PUBLIC_CLIENT_SECRET,
-      },
+      }),
       {
         headers: {
           "Content-type": "application/x-www-form-urlencoded",
