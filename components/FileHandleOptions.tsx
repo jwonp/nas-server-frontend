0745eacc1831b000c60127da66313ac717ed70b7
Joowon <201611804@kyonggi.ac.kr>
2023-02-22 00:48:59 +0900

    test

--- components/FileHandleOptions.tsx
+++ components/FileHandleOptions.tsx
@@ -115,7 +115,7 @@ const FileHandleOptions = () => {
       link.setAttribute("download", `${username}.zip`);
       link.click();
       // link.remove();
-      document.body.removeChild(link);
+      // document.body.removeChild(link);
 
       // 다운로드가 끝난 리소스(객체 URL)를 해제합니다.
       window.URL.revokeObjectURL(fileObjectUrl);
