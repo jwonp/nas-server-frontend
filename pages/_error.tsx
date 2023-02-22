7263f7fe25717612863dcc8d4aabae31b8abe0f9
Joowon <201611804@kyonggi.ac.kr>
2023-02-22 13:15:42 +0900

    update error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -3,10 +3,7 @@ import styles from "../styles/_error.module.css";
 function Error({ statusCode }) {
   return (
     <div className={`${styles.wrapper}`}>
-      <div className={`${styles.warning}`}>
-        해당 요청을 처리할 수 없습니다.
-        {statusCode ? ` || Error ${statusCode}` : ""}
-      </div>
+      <div className={`${styles.warning}`}>해당 요청을 처리할 수 없습니다.</div>
       <div className={`${styles.button}`}>
         <Link href={"/storage/내_드라이브"}>메인으로 돌아가기</Link>
       </div>
