9c98f3f119736837e3f02c984664eba2fc8389d4
Joowon <201611804@kyonggi.ac.kr>
2023-02-23 02:22:26 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import styles from "../styles/Home.module.css";
 const Home = () => {
   return (
-    <div>
-      <div>환영합니다.</div>
-      <div>좌측 상단의 로그인을 클릭하세요.</div>
+    <div className={`${styles.wrapper}`}>
+      <div className={`${styles.item}`}>환영합니다.</div>
+      <div className={`${styles.item}`}>좌측 상단의 로그인을 클릭하세요.</div>
+      <div className={`${styles.item}`}>
+        게스트 계정은 생성 제한이 있습니다.
+      </div>
+      <div className={`${styles.item}`}>
+        회원 가입이 안 될 경우에는 <h3>all_time_low@naver.com</h3>로
+        연락해주세요.
+      </div>
     </div>
   );
 };
