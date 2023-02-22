53896fc0790081b6e61eddebcce9379bd423d601
Joowon <201611804@kyonggi.ac.kr>
2023-02-22 13:13:50 +0900

    update error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -5,7 +5,7 @@ function Error({ statusCode }) {
     <div className={`${styles.wrapper}`}>
       <div className={`${styles.warning}`}>
         해당 요청을 처리할 수 없습니다.
-        {statusCode ? `  Error ${statusCode}` : ""}
+        {statusCode ? ` || Error ${statusCode}` : ""}
       </div>
       <div className={`${styles.button}`}>
         <Link href={"/storage/내_드라이브"}>메인으로 돌아가기</Link>
